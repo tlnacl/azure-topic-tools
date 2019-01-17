d45ff12472e6ac44647ca26e6cbe1cea379651f5
Liang Tang <liang.tang@coretex.com>
2019-01-17 17:40:37 +1300

    add test

---
+++ TopicTools.Tests/SendMessageTest.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using TopicTools;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TopicTools.Tests
+{
+    public class SendMessageTest
+    {
+        private readonly ILogger logger = NullLoggerFactory.Instance.CreateLogger("Test");
+
+        [Fact]
+        public void CorrectMessage()
+        {
+            string correctMsg = "{\"topicConnection\":\"topic-connection\",\"topicName\":\"topic-name\",\"label\":\"label\",\"message\":\"test\"}";
+            var request = GenerateHttpRequest(correctMsg);
+            var response = SendMessage.Run(request, logger).Result;
+            Assert.IsType<OkObjectResult>(response);
+            Assert.Equal("OK", ((OkObjectResult)response).Value as string);
+        }
+
+        [Fact]
+        public void EmptyMessage()
+        {
+            string emptyMsg = "";
+            var request = GenerateHttpRequest(emptyMsg);
+            var response = SendMessage.Run(request, logger).Result;
+            Assert.IsType<BadRequestObjectResult>(response);
+        }
+
+        private DefaultHttpRequest GenerateHttpRequest(string msg)
+        {
+            var request = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Body = new MemoryStream(Encoding.UTF8.GetBytes(msg ?? ""))
+            };
+            return request;
+        }
+    }
+}
